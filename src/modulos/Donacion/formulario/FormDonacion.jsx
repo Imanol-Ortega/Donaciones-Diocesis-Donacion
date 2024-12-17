b39cfc35dfdbad19989d201f4241f845dd5cf0be
Imanol-Ortega <imanicoortemohr018@gmail.com>
2024-12-17 17:31:36 -0300

    probando toast

--- src/modulos/Donacion/formulario/FormDonacion.jsx
+++ src/modulos/Donacion/formulario/FormDonacion.jsx
@@ -75,7 +75,6 @@ function FormDonacion() {
                 setPosition(null);
                 setPositionLoad([-27.338697141418727, -55.86717871248513]);
                 toastSucess();
-                reset();
             }
         } catch (error) {
             if (error.name == "AxiosError") {
